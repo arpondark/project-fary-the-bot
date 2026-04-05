bceceefa3180f0a9f7ba68f12415615ef48cdd08
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-04-05 20:30:07 +0600

    v1

---
+++ faribaclone.ino
@@ -0,0 +1,161 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+int leftX       = 10;   
+int rightX      = 118;  
+int eyeH        = 22;   
+int eyeFrame    = 0;    
+
+int eyeYOffset  = 0;   
+int pupilYShift = -2;  
+
+unsigned long lastTrigger = 0;
+
+// ---------------- EYE BLINK ----------------
+void tickEyes() {
+  eyeFrame++;
+  int phase = eyeFrame % 120; 
+
+  if (phase < 100) {
+    eyeH = 22; 
+  } else if (phase < 110) {
+    eyeH = 22 - ((phase - 100) * 2.2); 
+  } else {
+    eyeH = (phase - 110) * 2.2; 
+  }
+}
+
+// ---------------- HANDS ----------------
+void drawLeftHand(int x, int y) {
+  display.fillRoundRect(x, y + 5, 12, 8, 2, SSD1306_WHITE);      
+  display.fillRoundRect(x + 8, y + 5, 12, 3, 1, SSD1306_WHITE);  
+  display.fillRoundRect(x + 2, y + 2, 5, 4, 2, SSD1306_WHITE);   
+}
+
+void drawRightHand(int x, int y) {
+  display.fillRoundRect(x - 12, y + 5, 12, 8, 2, SSD1306_WHITE); 
+  display.fillRoundRect(x - 20, y + 5, 12, 3, 1, SSD1306_WHITE); 
+  display.fillRoundRect(x - 7, y + 2, 5, 4, 2, SSD1306_WHITE);   
+}
+
+// ---------------- FACE ----------------
+void drawFace() {
+  display.clearDisplay();
+
+  int baseEyeY   = 28 + eyeYOffset;
+  int baseMouthY = 46;
+
+  // eyebrows
+  display.drawLine(25, baseEyeY - 14, 42, baseEyeY - 18, SSD1306_WHITE); 
+  display.drawLine(86, baseEyeY - 18, 103, baseEyeY - 14, SSD1306_WHITE); 
+
+  int lEx = 42, rEx = 86, eyeR = 11;
+
+  // eyes
+  display.fillCircle(lEx, baseEyeY, eyeR, SSD1306_WHITE);
+  display.fillCircle(rEx, baseEyeY, eyeR, SSD1306_WHITE);
+
+  // blink mask
+  if (eyeH < 22) {
+    int maskH = (22 - eyeH) / 2;
+    display.fillRect(lEx-12, baseEyeY-12, 24, maskH, SSD1306_BLACK);
+    display.fillRect(lEx-12, baseEyeY+12-maskH, 24, maskH, SSD1306_BLACK);
+    display.fillRect(rEx-12, baseEyeY-12, 24, maskH, SSD1306_BLACK);
+    display.fillRect(rEx-12, baseEyeY+12-maskH, 24, maskH, SSD1306_BLACK);
+  }
+
+  // pupils
+  if (eyeH > 8) {
+    display.fillCircle(lEx + 3, baseEyeY + pupilYShift, 4, SSD1306_BLACK);
+    display.fillCircle(rEx - 3, baseEyeY + pupilYShift, 4, SSD1306_BLACK);
+    display.fillCircle(lEx + 1, baseEyeY + pupilYShift - 1, 2, SSD1306_WHITE);
+    display.fillCircle(rEx - 5, baseEyeY + pupilYShift - 1, 2, SSD1306_WHITE);
+  }
+
+  // mouth (fixed)
+  display.drawPixel(64, baseMouthY + 1, SSD1306_WHITE); 
+  display.drawLine(61, baseMouthY + 2, 63, baseMouthY + 2, SSD1306_WHITE);
+  display.drawLine(65, baseMouthY + 2, 67, baseMouthY + 2, SSD1306_WHITE);
+  display.drawPixel(60, baseMouthY, SSD1306_WHITE);
+  display.drawPixel(68, baseMouthY, SSD1306_WHITE);
+
+  // cheek dots
+  display.drawPixel(22, baseEyeY + 10, SSD1306_WHITE); 
+  display.drawPixel(106, baseEyeY + 10, SSD1306_WHITE);
+
+  // hands (MOVED DOWN)
+  drawLeftHand(leftX, 46);
+  drawRightHand(rightX, 46);
+
+  display.display();
+}
+
+// ---------------- ANIMATION ----------------
+void animateHands() {
+
+  // move in
+  for (int i = 0; i < 38; i++) {
+    if (leftX < 52) leftX++;
+    if (rightX > 76) rightX--;
+
+    if (i % 8 == 0) {
+      eyeYOffset--;
+      pupilYShift--;
+    }
+
+    if (eyeYOffset < -6) eyeYOffset = -6;
+    if (pupilYShift < -5) pupilYShift = -5;
+
+    tickEyes();
+    drawFace();
+    delay(40);
+  }
+
+  delay(1500);
+
+  // move out
+  for (int i = 0; i < 38; i++) {
+    if (leftX > 10) leftX--;
+    if (rightX < 118) rightX++;
+
+    if (i % 10 == 0) {
+      eyeYOffset++;
+      pupilYShift++;
+    }
+
+    if (eyeYOffset > 0) eyeYOffset = 0;
+    if (pupilYShift > -2) pupilYShift = -2;
+
+    tickEyes();
+    drawFace();
+    delay(30);
+  }
+}
+
+// ---------------- SETUP ----------------
+void setup() {
+  Wire.begin(21, 22);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    for(;;);
+  }
+  display.clearDisplay();
+  display.display();
+}
+
+// ---------------- LOOP ----------------
+void loop() {
+  tickEyes();
+  drawFace();
+  delay(30);
+
+  if (millis() - lastTrigger >= 3000) {
+    animateHands();
+    lastTrigger = millis();
+  }
+}
